d1c3a3a7a1e29ac9b3c9c21ff39a36affa1c0741
Shane Rich <srichnz@gmail.com>
2020-12-06 22:16:16 +0100

    updates AoCDay6

--- day6_part2.py
+++ day6_part2.py
@@ -1,15 +1,11 @@
-
-
 def question_count(group):
-    group_set = set(group)
-    n_set = set('\n')
-    group_set = group_set - n_set
+    group_set = set(group.replace(' ', ''))
     length = len(group_set)
     return length
 
 
 def everyone_yes(group):
-    persons = group.split('\n')
+    persons = group.split(' ')
     set_list = []
     for person in persons:
         person_set = set(person)
@@ -19,6 +15,7 @@ def everyone_yes(group):
 
 
 with open('day6.txt', mode='r') as f:
-    data = [(x.strip()) for x in f.read().split("\n\n")]
+    #data = [(x.strip()) for x in f.read().split("\n\n")]
+    data = ' '.join(f.read().splitlines()).split('  ')
     print(f'Part 1: {sum(question_count(group) for group in data)}')
     print(f'Part 2: {sum(everyone_yes(group) for group in data)}')

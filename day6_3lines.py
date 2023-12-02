296555bb52642e7d350f73dd03861f7eea1615ab
Shane Rich <shanerich@Shanes-MacBook-Pro.local>
2023-12-02 20:29:17 +1300

    Clean up files.

--- day6_3lines.py
+++
@@ -1,5 +0,0 @@
-in_ = ' '.join(open('day6.txt').read().splitlines()).split('  ')
-
-
-print(sum(len(set(a.replace(' ', ''))) for a in in_))
-print(sum(len(set.intersection(*[set(x) for x in a.split()])) for a in in_))

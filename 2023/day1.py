6c9f24bbfa4a3c9b0049d92509ef22cd0dc5b59d
Shane Rich <srichnz@gmail.com>
2023-12-04 09:45:44 +1300

    Day one part 2 solution.

--- 2023/day1.py
+++ 2023/day1.py
@@ -3,6 +3,17 @@ import re
 
 os.chdir("/Users/shanerich/AdventOfCode/2023")
 
+number_map = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
 
 def extract_first_and_last_numbers(text):
     pattern = r"\d+"
@@ -18,11 +29,20 @@ def extract_first_and_last_numbers(text):
 def calculate_sum(data):
     sum = 0
     for i in range(len(data)):
+        #comment out replace_words_with_numbers for part 1
+        replace_words_with_numbers(i, data)
         data[i] = [extract_first_and_last_numbers(x) for x in data[i]]
         flattened_data = [item for sublist in data for item in sublist]
         number_to_add = flattened_data[i][0][0] + flattened_data[i][1][-1]
         sum += int(number_to_add)
     return sum
+
+
+def replace_words_with_numbers(i, data):
+    for key in number_map:
+        # replacement adds the key back either side of the number so that spelling is not broken on other number words.
+        replacement = key + str(number_map[key]) + key
+        data[i] = [x.replace(key, replacement) for x in data[i]]
 
 with open("./day1.txt") as f:
     data = [x.strip().split(',') for x in f.read().splitlines()]
